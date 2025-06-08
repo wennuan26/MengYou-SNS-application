17984da7cc406660da1ff882ea48d1e07020cef6
cheiZh嗷 <zhaomingli26@gmail.com>
2025-06-08 10:11:25 +0530

    Create guiCons.java

---
+++ views/guiCons.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package views;
+
+import java.awt.Color;
+
+public class guiCons {
+    public static final Color Black = Color.decode("#1C1C1C");
+    public static final Color White = Color.WHITE;
+    public static final Color Blue = Color.decode("#02A1FC");
+    public static final Color Hint = Color.decode("#AAAAAA");
+    public static final Color Background = Color.decode("#F5F5F5");
+    public static final Color Accent = Color.decode("#4A90E2");
+    public static final Color SoftGray = Color.decode("#2E2E2E");
+}
