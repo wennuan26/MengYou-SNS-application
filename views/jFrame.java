92f7e34e084a0afd49be60def812bfe93a333178
cheiZh嗷 <zhaomingli26@gmail.com>
2025-06-08 10:11:28 +0530

    Create jFrame.java

---
+++ views/jFrame.java
@@ -0,0 +1,13 @@
+package views;
+
+@SuppressWarnings("serial")
+public class jFrame extends javax.swing.JFrame {
+    public jFrame() {
+        super("MengYou梦友");
+        getContentPane().setBackground(guiCons.Black);
+        setSize(900, 625);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setUndecorated(false); // remove if you want borderless
+    }
+}
