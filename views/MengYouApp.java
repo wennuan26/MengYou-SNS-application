f00b73fc9c81ef6193f9c26e6776eb7666e4e3fa
cheiZh嗷 <zhaomingli26@gmail.com>
2025-06-25 11:41:03 +0530

    Create MengYouApp.java

---
+++ views/MengYouApp.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package views;
+
+public class MengYouApp {
+    public static void main(String[] args) {
+        new LoginUI(); // Start with the login screen
+    }
+}
